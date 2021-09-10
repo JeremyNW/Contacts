b333ef80485db350b3370f04b2ac37f9e69fccea
JeremyNW <jeddynwarren@gmail.com>
2021-09-10 07:37:06 -0600

    inital commit

---
+++ Contacts/Model/Contact.swift
@@ -0,0 +1,33 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Jeremy Warren on 9/10/21.
+//
+
+import Foundation
+
+class Contact: Codable {
+    let name: String
+    let email: String
+    let phone: Int
+    let address: String
+    
+    init(name: String, email: String, phone: Int, address: String) {
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
+    }
+}
+
+extension Contact: Equatable {
+    static func == (lhs: Contact, rhs: Contact) -> Bool {
+        return lhs.name == rhs.name &&
+            lhs.email == lhs.email &&
+            lhs.phone == rhs.phone
+        lhs.address == rhs.address
+    }
+    
+    
+}
